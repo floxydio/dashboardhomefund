0183416b8e2a29cc093e603b9f6b60d81a25f0d5
floxydio <diooktar@gmail.com>
2023-12-06 10:44:57 +0700

    change url

--- src/components/AxiosConfig.jsx
+++ src/components/AxiosConfig.jsx
@@ -5,7 +5,7 @@ const apiPrefix = localStorage.getItem("apiPrefix")
 console.log(apiPrefix)
 
 const axiosNew = axios.create({
-  baseURL: `https://aa10-2400-9800-3d0-d74e-8c1a-465a-e7b1-95a8.ngrok-free.app/dashboard-api`,
+  baseURL: `https://7a2a-114-124-214-20.ngrok-free.app/dashboard-api`,
   headers: {
     "ngrok-skip-browser-warning": "any"
   },
